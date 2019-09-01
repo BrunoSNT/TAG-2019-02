d32ede40a536ce3ec944dc7f3ef72511af3175a1
NayRSilva <nayarasilva@cjr.org.br>
2019-09-01 19:55:37 -0300

    feature Nayara

--- constroi.cpp
+++ constroi.cpp
@@ -1,71 +1,58 @@
-    #include <stdio.h>
-    #include<vector>
-    #include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <iostream>
 #include <list>
 #include <algorithm> // função find
+#include "constroi.h"
 
 using namespace std;
 
-    class Grafo
-    {
-	    int vertice; // número de vértices
-	    list<int> *adjacencia; // ponteiro para um array contendo as listas de adjacências
+Grafo criaGrafo(void)
+{
+    Grafo grafo(63); //O grafo vai de 0 a 62, precisamos de 63 lugaresinhos bonitinhos
+    FILE *in = fopen("soc-dolphins.mtx", "r");
+    vector<int> valores;
 
-        public:
-	    Grafo(int Vertice); // construtor
-        void addAresta(int vertice, int v);
-	
-    };
-
-Grafo::Grafo(int vertice)
+    if (in != NULL)
     {
-    	this->vertice = vertice; // atribui o número de vértices
-    	adjacencia = new list<int>[vertice]; // cria as listas
+        char line[BUFSIZ];
+
+        while (fgets(line, sizeof line, in) != NULL)
+        {
+            char *start = line;
+            int field;
+            int n;
+            int vertice;
+            if (line[0] == '%')
+                continue;
+            //Aqui iremos usar o vetor valores para armazenar todos os valores de uma linha, para depois podermos adicionar esses valores na lista (tem que ter um jeito melhor de fazer isso, btw)
+            while (sscanf(start, "%d%n", &field, &n) == 1)
+            {
+
+                valores.push_back(field); //envia o valor que leu na linha para o vetor
+                start += n;               //isso aqui é o que faz ele mudar de linha so quando acabar de ler a linha
+            }
+            for (unsigned int i = 1; i < valores.size(); i++)//o valores[0] é a onde eu adiciono meu valor, assim eu nao começo do indice 0, mas do indice 1
+            {
+                grafo.addAresta(valores[0], valores[i]); //se o elemento 11 tem 1 como vizinho, adiciona 1 a lista de 11
+                if(!grafo.existeElemento(valores[0], valores[i])){
+                    grafo.addAresta(valores[i], valores[0]);//se o elemento 1 tem 11 como vizinho, adiciona 11 a lista de 1.
+                }
+            }
+            vertice++;
+            valores.clear();
+        }   
+
+        fclose(in);
     }
 
-void Grafo::addAresta(int vertice, int v){
-    adjacencia[vertice].push_back(v);
+    return grafo;
 }
 
- 
-    int main(void)
-    {
-        Grafo grafo(63);//O grafo vai de 0 a 62, precisamos de 63 lugaresinhos bonitinhos
-        FILE *in = fopen("soc-dolphins.mtx", "r");
-        vector <int> valores;
-        
-        if (in != NULL) {
-            char line[BUFSIZ];
-            
-            while (fgets(line, sizeof line, in) != NULL) {
-                char *start = line;
-                int field;
-                int n;
-                int primeiro;
-                int contador;
-                int vizinho =1;
-                int vertice;
-                if(line[0]=='%') continue;
-                   contador = 0;
-                //Aqui iremos usar o vetor valores para armazenar todos os valores de uma linha, para depois podermos adicionar esses valores na lista (tem que ter um jeito melhor de fazer isso, btw)
-                while (sscanf(start, "%d%n", &field, &n) == 1) {
-                  
-                valores.push_back(field);//envia o valor que leu na linha para o vetor
-                    start += n;//isso aqui é o que faz ele mudar de linha so quando acabar de ler a linha
-               
-                }
-                for(int i=1; i< valores.size(); i++){
-                    grafo.addAresta(valores[0], valores[i]);
-                    printf("valor %d vertice %d", valores[0], valores[i]);
-                }
-                    vertice ++;
-                    contador = 0; 
-                    valores.clear();
-                puts("");
-            }
-            
-            fclose(in);
-        }
-        
-        return 0;
-    }
+int main(void)
+{
+    Grafo g = criaGrafo();
+    g.printVizinhos(63);
+
+    return 0;
+}
---
+++ constroi.h
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+class Grafo
+{
+    int vertice;           // número de vértices
+    list<int> *adjacencia; // ponteiro para um array contendo as listas de adjacências
+
+public:
+    Grafo(int Vertice); // construtor
+    void addAresta(int vertice, int v);
+    void printVizinhos(int n);
+    bool Grafo::coefAglomeracao(int vertice, Grafo grafo);
+    bool existeElemento(int elemento, int vertice);
+    bool eVizinho(int vertice, int elemento);
+};
+Grafo::Grafo(int vertice)
+{
+    this->vertice = vertice;             // atribui o número de vértices
+    adjacencia = new list<int>[vertice]; // cria as listas
+}
+void Grafo::addAresta(int vertice, int v)
+{
+    adjacencia[vertice].push_back(v);
+}
+void Grafo::printVizinhos(int n){
+    for(int i =1; i<n; i++){
+        printf("vertice %d vizinhos: \n",i);
+        for( list<int>::iterator iterador = adjacencia[i].begin(); iterador != adjacencia[i].end(); iterador ++){
+            
+            cout << " " << *iterador;
+            cout << "\n";
+        }
+    }
+
+}
+
+
+bool Grafo::coefAglomeracao(int vertice, Grafo grafo){
+        int triangulos = 0;
+        int numVizinhos = adjacencia[vertice].size();
+        int paresDistintos;
+        int coeficiente;
+    for( list<int>::iterator iterador = adjacencia[vertice].begin(); iterador != adjacencia[vertice].end(); iterador ++){
+        int aux;
+        aux = *iterador;
+        list<int>::iterator it = iterador ++;
+        for(it; it!= adjacencia[vertice].end(); it++){
+            int elemento = *it;
+            if(grafo.existeElemento(aux, elemento)){
+                triangulos ++;
+            };
+        }
+    }    
+    paresDistintos = numVizinhos * ((numVizinhos-1)/2);
+    coeficiente = triangulos/paresDistintos;
+
+
+}
+
+bool Grafo::existeElemento(int elemento, int vertice){
+  if (adjacencia[vertice].size()>0){
+      list<int>::iterator it;
+        it = std::find(adjacencia[vertice].begin(), adjacencia[vertice].end(), elemento);
+ 
+    if(it != adjacencia[vertice].end()){
+
+        getchar();
+        return true;        cout<< "o elemento" << elemento << "ja esta na lista";
+        }
+        else{ 
+            return false;
+        
+        };
+
+  }
+    return false;
+
+}
